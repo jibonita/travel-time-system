a439b94c99bcd881b3bff957e9d0f0cbfc9b0d89
Osman Yumer <osman.yumer@abv.bg>
2019-01-21 21:38:20 +0200

    added register tech user component

---
+++ client/src/app/admin/users/register-tech-user/register-tech-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-tech-user',
+  templateUrl: './register-tech-user.component.html',
+  styleUrls: ['./register-tech-user.component.css']
+})
+export class RegisterTechUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
