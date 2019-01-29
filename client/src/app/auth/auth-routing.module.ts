4b71e73205a08bc9a123aee6442aa7341373eaaf
jibonita <bamishore@gmail.com>
2019-01-29 09:37:07 +0200

    moved SentenceCasePipe to SharedModule

--- client/src/app/auth/auth-routing.module.ts
+++ client/src/app/auth/auth-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-//import { MaterialSharedModule } from '../shared/material-shared.module';
 import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './login/login.component';
 import { AnonymousRouteActivatorService } from '../core/route-guards/anonymous-route-activator.service';
-import { SentenceCasePipe } from '../pipes/sentence-case.pipe';
 import { RegisterComponent } from './register/register.component';
 import { LengthDirective } from '../directives/length.directive';
 
@@ -26,7 +24,6 @@ import { LengthDirective } from '../directives/length.directive';
 @NgModule({
   imports: [
     CommonModule,
-    //MaterialSharedModule,
     SharedModule,
     ReactiveFormsModule,
     RouterModule.forChild(ROUTES),
@@ -34,7 +31,6 @@ import { LengthDirective } from '../directives/length.directive';
   declarations: [
     LoginComponent,
     RegisterComponent,
-    SentenceCasePipe,
     LengthDirective,
                 ],
 })
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { МodalComponent } from './modal/modal.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { StreetMapComponent } from './street-map/street-map.component';
+import { SentenceCasePipe } from '../pipes/sentence-case.pipe';
 
 @NgModule({
-  declarations: [МodalComponent, SearchBarComponent, StreetMapComponent],
+  declarations: [МodalComponent, SearchBarComponent, StreetMapComponent,SentenceCasePipe],
   imports: [CommonModule],
-  exports: [CommonModule, МodalComponent, SearchBarComponent, StreetMapComponent]
+  exports: [CommonModule, МodalComponent, SearchBarComponent, StreetMapComponent, SentenceCasePipe]
 })
 export class SharedModule {}
