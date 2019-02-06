e4686d962c4d1729817631e4ab732923f4c29651
jibonita <bamishore@gmail.com>
2019-02-06 12:32:56 +0200

    added street-route component to hold the routes map

---
+++ client/src/app/reports/street-route/street-route.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MapService } from 'src/app/core/map.service';
+import { ReportDataListenerService } from '../services/report-data-listener.service';
+
+@Component({
+  selector: 'app-street-route',
+  templateUrl: './street-route.component.html',
+  styleUrls: ['./street-route.component.css']
+})
+export class StreetRouteComponent implements OnInit, OnDestroy {
+
+  reportDataListenerSubscriber;
+
+  constructor(
+    private readonly mapService: MapService,
+    private readonly reportDataListenerService: ReportDataListenerService
+  ) { }
+
+  ngOnInit() {
+    
+    this.reportDataListenerSubscriber =
+      this.reportDataListenerService.currentRouteDraw$.subscribe(
+        (data) => {
+            if(data.length > 0){
+              this.mapService.clearRoutes();
+              
+              this.mapService.showRoute(data);
+            }
+            
+        }
+      );
+  }
+
+
+  ngOnDestroy(){
+    
+  }
+}
